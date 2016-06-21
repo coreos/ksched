7f2b7ed8adb47d16a9768902dfa621d88f788277
Haseeb Tariq <haseeb.tariq@coreos.com>
2016-06-21 11:28:34 -0700

    pkg rename: graph->cluster

--- scheduling/flow/cluster/graph.go
+++ scheduling/flow/cluster/graph.go
@@ -15,7 +15,7 @@
 // Represents the scheduling flow graph.
 // C++ file: https://github.com/camsas/firmament/blob/master/src/scheduling/flow/flow_graph.h
 
-package graph
+package cluster
 
 import (
 	"log"
