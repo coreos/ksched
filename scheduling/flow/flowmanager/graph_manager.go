b592cf723ce44bbcc60d2412ebdfa741c8ea4b37
Xiang Li <xiangli.cs@gmail.com>
2016-06-22 20:00:52 -0700

    flowmanager: fill in graph manager interface

--- scheduling/flow/flowmanager/graph_manager.go
+++ scheduling/flow/flowmanager/graph_manager.go
@@ -14,8 +14,55 @@
 
 package flowmanager
 
+import (
+	"github.com/coreos/ksched/pkg/types"
+	pb "github.com/coreos/ksched/proto"
+)
+
 // NOTE: GraphManager uses GraphChangeManager to change the graph.
 type GraphManager interface {
+	AddOrUpdateJobNodes(jobs []pb.JobDescriptor)
+
+	UpdateTimeDependentCosts(jobs []pb.JobDescriptor)
+
+	// AddResourceTopology adds the entire resource topology tree. The method
+	// also updates the statistics of the nodes up to the root resource.
+	AddResourceTopology(topo pb.ResourceTopologyNodeDescriptor)
+
+	UpdateResourceTopology(topo pb.ResourceTopologyNodeDescriptor)
+
+	// TODO: ComputeTopologyStatistics(...)
+
+	JobCompleted(id types.JobID)
+
+	NodeBindingToSchedulingDeltas(
+		taskNodeID, resourceNodeID uint64,
+		taskBindings map[types.TaskID]types.ResourceID,
+		deltas []pb.SchedulingDelta)
+
+	SchedulingDeltasForPreemptedTasks(taskMapping map[uint64]uint64, rmap types.ResourceMap, deltas []pb.SchedulingDelta)
+
+	// As a result of task state change, preferences change or
+	// resource removal we may end up with unconnected equivalence
+	// class nodes. This method makes sure they are removed.
+	// We cannot end up with unconnected unscheduled agg nodes,
+	// task or resource nodes.
+	PurgeUnconnectedEquivClassNodes()
+
+	//  Removes the entire resource topology tree rooted at rd. The method also
+	//  updates the statistics of the nodes up to the root resource.
+	RemoveResourceTopology(rd pb.ResourceDescriptor) (removedPUs []uint64)
+
+	TaskCompleted(id types.TaskID) uint64
+	TaskEvicted(id types.TaskID, rid types.ResourceID)
+	TaskFailed(id types.TaskID)
+	TaskKilled(id types.TaskID)
+	TaskMigrated(id types.TaskID, from, to types.ResourceID)
+	TaskScheduled(id types.TaskID, rid types.ResourceID)
+
+	// Update each task's arc to its unscheduled aggregator. Moreover, for
+	// running tasks we update their continuation costs.
+	UpdateAllCostsToUnscheduledAggs()
 }
 
 type graphManager struct {
