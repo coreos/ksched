a95d2bc7d77bc4e2569b0f19a90549a2a36763a2
Xiang Li <xiangli.cs@gmail.com>
2016-06-23 17:33:17 -0700

    flowmanager: add AddOrUpdateJobNodes

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -17,3 +17,11 @@ func ResourceIDFromString(s string) (types.ResourceID, error) {
 	i, err := strconv.ParseUint(s, 10, 64)
 	return types.ResourceID(i), err
 }
+
+func MustJobIDFromString(s string) types.JobID {
+	i, err := strconv.ParseUint(s, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return types.JobID(i)
+}
