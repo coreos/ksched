40abf99603363a9d6bff21041e1d24f0f7536bd7
Hongchao Deng <hongchaodeng1@gmail.com>
2016-06-24 14:48:19 -0700

    dimacs: add Export(), ExportIncremental()

---
+++ scheduling/flow/dimacs/export.go
@@ -0,0 +1,70 @@
+package dimacs
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/coreos/ksched/scheduling/flow/flowgraph"
+)
+
+func Export(g *flowgraph.Graph, w io.Writer) {
+	fmt.Fprint(w, "c ===========================\n")
+	fmt.Fprintf(w, "p min %d %d\n", g.NumNodes(), g.NumArcs())
+	fmt.Fprint(w, "c ===========================\n")
+
+	fmt.Fprint(w, "c === ALL NODES FOLLOW ===\n")
+	for _, n := range g.Nodes() {
+		generateNode(n, w)
+	}
+
+	fmt.Fprint(w, "c === ALL ARCS FOLLOW ===\n")
+	for _, r := range g.Arcs() {
+		generateArc(r, w)
+	}
+
+	fmt.Fprintf(w, "c EOI\n")
+}
+
+func ExportIncremental(changes []Change, w io.Writer) {
+	for _, change := range changes {
+		fmt.Fprint(w, change.GenerateChange())
+	}
+}
+
+func generateNode(n *flowgraph.Node, w io.Writer) {
+	// dimacs comments
+	switch {
+	case n.ResourceDescriptor != nil:
+		fmt.Fprint(w, "c nd Res_%s\n", n.ResourceDescriptor.Uuid)
+	case n.Task != nil:
+		fmt.Fprint(w, "c nd Task_%d\n", n.Task.Uid)
+	case n.EquivClass != nil:
+		fmt.Fprint(w, "c nd EC_%d\n", *n.EquivClass)
+	case n.Comment != "":
+		fmt.Fprint(w, "c nd %s\n", n.Comment)
+	}
+
+	// node type used by solver.
+	// default is 0.
+	// In the future, we should have common defs between both sides.
+	nodeType := 0
+	switch n.Type {
+	case flowgraph.NodeTypePu:
+		nodeType = 2
+	case flowgraph.NodeTypeMachine:
+		nodeType = 4
+	case flowgraph.NodeTypeNuma, flowgraph.NodeTypeSocket, flowgraph.NodeTypeCache, flowgraph.NodeTypeCore:
+		nodeType = 5
+	case flowgraph.NodeTypeSink:
+		nodeType = 3
+	case flowgraph.NodeTypeUnscheduledTask, flowgraph.NodeTypeScheduledTask, flowgraph.NodeTypeRootTask:
+		nodeType = 1
+	}
+
+	fmt.Fprintf(w, "n %d %d %d\n", n.ID, n.Excess, nodeType)
+}
+
+func generateArc(arc *flowgraph.Arc, w io.Writer) {
+	fmt.Fprintf(w, "a %d %d %d %d %d\n",
+		arc.Src, arc.Dst, arc.CapLowerBound, arc.CapUpperBound, arc.Cost)
+}
