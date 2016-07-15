763b43408b21d92cb94acbd42246a2a3c3f37483
Haseeb Tariq <haseeb.tariq@coreos.com>
2016-07-15 12:06:22 -0700

    bug-fix: range of slice

--- scheduling/flow/flowmanager/graph_change_manager.go
+++ scheduling/flow/flowmanager/graph_change_manager.go
@@ -15,6 +15,8 @@
 package flowmanager
 
 import (
+	"log"
+
 	"github.com/coreos/ksched/scheduling/flow/dimacs"
 	"github.com/coreos/ksched/scheduling/flow/flowgraph"
 )
@@ -97,6 +99,8 @@ func (cm *changeManager) AddArc(src, dst *flowgraph.Node,
 	changeType dimacs.ChangeType,
 	comment string) *flowgraph.Arc {
 
+	log.Printf("Change:%s - Added arc from src(%s):%d to dst(%s):%d", comment, src.Type, src.ID, dst.Type, dst.ID)
+
 	arc := cm.flowGraph.AddArc(src, dst)
 	arc.CapLowerBound = capLowerBound
 	arc.CapUpperBound = capUpperBound
@@ -116,6 +120,8 @@ func (cm *changeManager) AddNode(t flowgraph.NodeType, excess int64, changeType 
 	n.Excess = excess
 	n.Comment = comment
 
+	log.Printf("Change:%s - Added node(%s):%d", comment, n.Type, n.ID)
+
 	change := dimacs.NewAddNodeChange(n)
 	change.SetComment(comment)
 	cm.addGraphChange(change)
@@ -125,6 +131,7 @@ func (cm *changeManager) AddNode(t flowgraph.NodeType, excess int64, changeType 
 
 func (cm *changeManager) DeleteNode(n *flowgraph.Node, changeType dimacs.ChangeType, comment string) {
 	cm.flowGraph.DeleteNode(n)
+	log.Printf("Change:%s - Deleted node(%s):%d", comment, n.Type, n.ID)
 
 	change := &dimacs.RemoveNodeChange{
 		ID: uint64(n.ID),
@@ -181,7 +188,7 @@ func (cm *changeManager) ChangeArcCost(arc *flowgraph.Arc, cost int64, changeTyp
 func (cm *changeManager) DeleteArc(arc *flowgraph.Arc, changeType dimacs.ChangeType, comment string) {
 	arc.CapUpperBound = 0
 	arc.CapLowerBound = 0
-
+	log.Printf("Change:%s - Deleted arc from src(%s):%d to dst(%s):%d", comment, arc.SrcNode.Type, arc.SrcNode.ID, arc.DstNode.Type, arc.DstNode.ID)
 	change := dimacs.NewUpdateArcChange(arc, arc.Cost)
 	change.SetComment(comment)
 	cm.addGraphChange(change)
