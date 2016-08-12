4be62cbbf4ad36f8888dbc217a495d361476ab58
Haseeb Tariq <haseeb.tariq@coreos.com>
2016-08-12 12:47:49 -0700

    newlines

--- build/init.sh
+++ build/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Clone the ksched repo, (for now it's a duplicate repo because of access issues with private repos)
+# Clone the ksched repo, (for now it's a mirror repo because of access issues with private coreos repo)
 cd /root/go-workspace/src/github.com/coreos && git clone https://github.com/hasbro17/ksched-mirror ksched
 cd ksched/proto
 ./genproto.sh
